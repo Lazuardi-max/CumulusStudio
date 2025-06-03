198f5bbceae8168a804d78d63c867340116c603b
Lazuardi-max <khamamlazuardi@gmail.com>
2025-06-03 14:56:57 +0700

    git commit -m

---
+++ js/script.js
@@ -0,0 +1,32 @@
+   // Fungsi toggle tema
+    function toggleDarkMode() {
+      document.body.classList.toggle('dark-mode');
+      const isDark = document.body.classList.contains('dark-mode');
+      localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    }
+
+    if(document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('mode', 'dark');
+    } else {
+        localStorage.setItem('mode', 'light');
+    }
+
+    // Cek preferensi tema saat halaman dimuat
+    window.onload = function() {
+        const savedMode = localStorage.getItem('mode');
+        if (savedMode === 'dark') {
+            document.body.classList.add('dark-mode');
+        }
+      applyTheme();
+    };
+
+    // Cek preferensi dari localStorage
+    const userTheme = localStorage.getItem('theme');
+    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    // Terapkan tema berdasarkan preferensi
+    if (userTheme === 'dark' || (!userTheme && systemPrefersDark)) {
+      document.body.classList.add('dark-mode');
+    }
+
+ 
